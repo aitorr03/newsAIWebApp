b5127eed4ed2cb67242dc6eb41d112a17f08d3f0
aitorr03 <118127866+aitorr03@users.noreply.github.com>
2025-04-07 21:18:52 +0200

    added libraries and imports for TW and React

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-500">
+        ¡Hola, React y Tailwind!
+      </h1>
+    </div>
   );
 }
 
