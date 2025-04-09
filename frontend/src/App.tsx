b79cb5b68f1265adebcec43ede6964444a94ac29
aitorr03 <118127866+aitorr03@users.noreply.github.com>
2025-04-09 18:32:22 +0200

    last imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-200">
-      <h1 className="text-4xl font-bold text-blue-500">
-        ¡Hola, React y Tailwind!
-      </h1>
-    </div>
-  );
+  return <Navbar />;
 }
 
 export default App;
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return <header className="bg-red sticky justify-between"></header>;
+};
+
+
+export default Navbar;
