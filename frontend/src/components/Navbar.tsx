5bfe7cc854e065e689b6bb5cc7f046f393ad1e23
aitorr03 <118127866+aitorr03@users.noreply.github.com>
2025-04-09 19:39:07 +0200

    fix tailwindcss dependencies

--- frontend/src/components/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const Navbar = () => {
-  return <header className="bg-red sticky justify-between"></header>;
-};
-
-
-export default Navbar;
