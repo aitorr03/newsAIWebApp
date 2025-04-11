e7d03d32e3bd254630f21c83075b8399fa3912c3
aitorr03 <118127866+aitorr03@users.noreply.github.com>
2025-04-11 21:08:16 +0200

    add Cors

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
 from backend.src.routers import jwt_auth_users as jwt, news_router
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 import os
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 load_dotenv()
 
 
